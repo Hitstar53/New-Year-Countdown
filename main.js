1a30012064eb28a7afd9c4a98eaa3bbe64b23815
Hatim Sawai <hatimsawai76@gmail.com>
2022-12-31 20:48:36 +0530

    final commit
    Signed-off-by: Hatim Sawai <hatimsawai76@gmail.com>

--- main.js
+++ main.js
@@ -3,8 +3,8 @@ let dayBox = document.getElementById("day-box");
 let hrBox = document.getElementById("hr-box");
 let minBox = document.getElementById("min-box");
 let secBox = document.getElementById("sec-box");
-//let endDate = new Date(2023, 0, 1, 00, 00);
-let endDate = new Date(2022, 11, 31, 20, 48);
+let endDate = new Date(2023, 0, 1, 00, 00);
+//let endDate = new Date(2022, 11, 31, 20, 48);
 let endTime = endDate.getTime();
 
 function countdown() {
